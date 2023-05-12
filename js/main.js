e7e7a8d5e5b68de6378681808e3bbf662cdbce43
MariaClaraBermudez <mariaclarabermudez10@gmail.com>
2023-05-11 23:49:20 -0300

    atualizei exercicio

--- js/main.js
+++ js/main.js
@@ -1,23 +1,55 @@
 const form = document.getElementById("novoItem")//trazer com id o formulario
 const lista = document.getElementById("lista") //receber id de lista
+const itens = []
 
-form.addEventListener("submit", (evento) => { //  os dados escritos são enviados após a ação de clicar no botão “Adicionar”
+form.addEventListener("submit", (evento) => {  //  os dados escritos são enviados após a ação de clicar no botão “Adicionar”
    evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+    nome.value = "" 
+    quantidade.value = ""
+    //limpar bloco de digitaçao automaticamente depois de enviar
 })
 
-function criaElemento(nome, quantidade) { //para receber novos produtos na lista
-      const novoItem = document.createElement('li') //criei um item em li(lista)
+function criaElemento(nome, quantidade) { 
+  //para receber novos produtos na lista
+
+      const novoItem = document.createElement('li') 
+      //cria um  li(lista) que guardara um novo item
+
     novoItem.classList.add("item") 
+    //dar uma classe para essa li criada acima
 
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade  //receber quantidade do produto
+    const numeroItem = document.createElement('strong') 
+    //cria um elemento(tag) strong para a quantidade, para ter o mesmo estilo do html (num ero em negrito)
+
+    numeroItem.innerHTML = quantidade  //receber quantidade do produto, o numeroItem ficara no campo de input Quantidade recebendo o valor numerico
 
 
-    novoItem.appendChild(numeroItem) //receber numero de item junto com nome do item
+    novoItem.appendChild(numeroItem) 
+    //infiltra um item (strong) dentro de outro no html ou seja adiciona strong ao novoItem criado
+
+
+
     novoItem.innerHTML += nome
+    //fazer com que o nome venha apos a quantidade dele 
+    //ex: 7 (quantidade) camisas cinzas (nome)
 
-    lista.appendChild(novoItem) // recebera produto de forma visual
+    lista.appendChild(novoItem) 
+    // adicionar tudo oq foi feito acima (novoItem) a pagina web, para quando der o submit (apertar o botao adicionar item) criar um novo item com a quantidade e nome,e css e html ja incluso
 
+
+    const itemAtual = {
+      "nome": nome,
+      "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))  //acessar e salvar informaçoes do usuario
+    
 }
+

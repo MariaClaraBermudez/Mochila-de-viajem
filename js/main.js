5f13c1d5bbdc199e863ca6c69d845faf3e59a407
MariaClaraBermudez <mariaclarabermudez10@gmail.com>
2023-05-21 23:48:28 -0300

    finalizei exercicio

--- js/main.js
+++ js/main.js
@@ -25,10 +25,12 @@ form.addEventListener("submit", (evento) => {  //  os dados escritos são enviad
         
         atualizaElemento(itemAtual)
 
-        itens[existe.id] = itemAtual
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
       } else { // se nao existir  e jogar elemento no array
 
-        itemAtual.id = itens.length //criar id para todos os elemento que estiver na lista
+        itemAtual.id = itens[itens.length -1] ? itens[itens.length-1].id + 1 : 0;
+        //se o array nao  existir da o id 0
+         //criar id para todos os elemento que estiver na lista
 
         criaElemento(itemAtual) //criar esse elemeto
     
@@ -39,8 +41,9 @@ form.addEventListener("submit", (evento) => {  //  os dados escritos são enviad
    
 
     
+      localStorage.setItem("itens", JSON.stringify(itens))  //acessar e salvar informaçoes do usuario
 
-    localStorage.setItem("itens", JSON.stringify(itens))  //acessar e salvar informaçoes do usuario
+   
     
     nome.value = "" 
     quantidade.value = ""
@@ -74,7 +77,7 @@ function criaElemento(item) {
     //fazer com que o nome venha apos a quantidade dele 
     //ex: 7 (quantidade) camisas cinzas (nome)
 
-    novoItem.appendChild(botaoDeleta()) //itens ter o botao de deletar
+    novoItem.appendChild(botaoDeleta(item.id)) //itens ter o botao de deletar
 
     lista.appendChild(novoItem) 
     // adicionar tudo oq foi feito acima (novoItem) a pagina web, para quando der o submit (apertar o botao adicionar item) criar um novo item com a quantidade e nome,e css e html ja incluso
@@ -85,16 +88,23 @@ function atualizaElemento(item) {
   document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
 
-function botaoDeleta() { //criar botao de deletar
+function botaoDeleta(id) { //criar botao de deletar
   const elementoBotao = document.createElement("button")
   elementoBotao.innerText = "X"
 
   elementoBotao.addEventListener("click", function(){
-    deletaElemento(this.parentNode) //remover elemento junto com o botao
+    deletaElemento(this.parentNode, id) //remover elemento junto com o botao
   })
 
   return elementoBotao
 }
-function deletaElemento(tag) {
+function deletaElemento(tag, id) {
   tag.remove () //funçao para remover tag
+ 
+
+  itens.splice(itens.findIndex(elemento => elemento.id === id), 1)     //escrever no localstorage
+
+
+ localStorage.setItem("itens", JSON.stringify(itens))  //acessar e salvar informaçoes do usuario
+
 }
